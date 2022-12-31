399d4e5639472d39699d9f40a560954a8e4c370b
SLOTH Jean-Michel <jeanmichelsloth@gmail.com>
2022-12-31 09:20:57 +0100

    maj config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './components/0-accueil.component';
+import { DecompteComponent } from './components/1-decompte.component';
+import { AlerteComponent } from './components/2-alerte.component';
+import { ConfigurationComponent } from './components/3-configuration.component';
+import { LancementComponent } from './components/4-lancement.component';
+import { ActiviteComponent } from './components/5-activite.component';
+import { BonneAnneeComponent } from './components/6-bonne-annee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'decompte', component: DecompteComponent },
+  { path: 'alerte', component: AlerteComponent },
+  { path: 'configuration', component: ConfigurationComponent },
+  { path: 'lancement', component: LancementComponent },
+  { path: 'activite', component: ActiviteComponent },
+  { path: 'bonne-annee', component: BonneAnneeComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'accueil' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
+import { registerLocaleData } from '@angular/common';
+import localeFrCa from '@angular/common/locales/fr-CA';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './components/0-accueil.component';
+import { DecompteComponent } from './components/1-decompte.component';
+import { AlerteComponent } from './components/2-alerte.component';
+import { ConfigurationComponent } from './components/3-configuration.component';
+import { LancementComponent } from './components/4-lancement.component';
+import { ActiviteComponent } from './components/5-activite.component';
+import { BonneAnneeComponent } from './components/6-bonne-annee.component';
+
+registerLocaleData(localeFrCa);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    DecompteComponent,
+    AlerteComponent,
+    ConfigurationComponent,
+    LancementComponent,
+    ActiviteComponent,
+    BonneAnneeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
